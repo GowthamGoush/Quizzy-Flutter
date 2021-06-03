c0900d228d7f466b69c27663918a0a28f071e695
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-06-03 10:34:20 +0530

    Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,132 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quizzy_flutter_app/quizData.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  scoreBoardSetUp();
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: QuizzyApp(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+  ));
+}
+
+class QuizzyApp extends StatefulWidget {
+  @override
+  _QuizzyAppState createState() => _QuizzyAppState();
+}
+
+QuizData quizData = new QuizData();
+
+List<Widget> scoreIcons = [];
+
+void scoreBoardSetUp() {
+  scoreIcons.clear();
+
+  for (int i = 0; i < quizData.getQuizLength(); i++) {
+    scoreIcons.add(Icon(
+      Icons.check_box_outline_blank,
+      color: Colors.grey,
+    ));
+  }
+}
+
+void buttonClicked(bool clickedAnswer) {
+  if (quizData.quizOver) {
+    return;
+  }
+
+  int quizNumber = quizData.getQuizNumber();
+
+  scoreIcons.removeAt(quizNumber);
+
+  if (clickedAnswer == quizData.getQuizAnswer()) {
+    scoreIcons.insert(
+        quizNumber,
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+  } else {
+    scoreIcons.insert(
+        quizNumber,
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+  }
+
+  quizData.nextQuestion();
+}
+
+class _QuizzyAppState extends State<QuizzyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Center(
+                child: Text(
+                  quizData.getQuizText(),
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      wordSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            )),
+        Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green)),
+                child: Text(
+                  'True',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                onPressed: () {
+                  setState(() {
+                    buttonClicked(true);
+                  });
+                },
+              ),
+            )),
+        Expanded(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red)),
+                child: Text(
+                  'False',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                onPressed: () {
+                  setState(() {
+                    buttonClicked(false);
+                  });
+                },
+              ),
+            )),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: scoreIcons)
+      ],
     );
   }
 }
